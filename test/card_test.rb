b0189aaa7a420967bdb948b4b8521c21fa8d7236
Nate Giesing <nategiesing@Nates-MacBook-Pro.local>
2017-06-12 11:27:32 -0600

    test the question working

--- test/card_test.rb
+++ test/card_test.rb
@@ -7,5 +7,10 @@ class CardTest < MiniTest::Test
     assert_instance_of Card, card
   end
 
+   def test_the_question
+     card = Card.new("What is the capital of Alaska", "Juneau")
+     expected = "What is the capital of Alaska?"
+     assert_equal expected, card.question
+ end
 
 end
