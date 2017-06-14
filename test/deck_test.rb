5fd7851996948ceaf0669f56aa078df416a3b786
Nate Giesing <nategiesing@Nates-MacBook-Pro.local>
2017-06-14 14:17:36 -0600

    finished iteration 3

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,12 +5,14 @@ class DeckTest < MiniTest::Test
 
   def test_it_exists
     deck = Deck.new("a")
+
     assert_instance_of Deck, deck
   end
 
   def test_it_can_store_cards
     card_1 = Card.new("What is the capital of Alaska?", "Juneau")
     deck = Deck.new([card_1])
+
     assert_equal [card_1], deck.cards
   end
 
@@ -19,7 +21,24 @@ class DeckTest < MiniTest::Test
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
     deck   = Deck.new([card_1, card_2, card_3])
+
     assert_equal [card_1, card_2, card_3], deck.cards
     assert_instance_of Array, deck.cards
+  end
+
+  def test_it_can_count_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck   = Deck.new([card_1, card_2, card_3])
+
+    assert_equal 3, deck.count
+  end
+
+  def test_it_can_one_count_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    deck   = Deck.new([card_1])
+    
+    assert_equal 1, deck.count
   end
 end
