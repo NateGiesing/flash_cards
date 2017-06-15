d58c79fa42ec64eec1127cef5429f63fe9fd5d64
Nate Giesing <nategiesing@Nates-MacBook-Pro.local>
2017-06-15 14:23:38 -0600

    finished still don't understand yet

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,16 @@
+require "./lib/card"
 class CardGenerator
+  attr_reader :cards
 
   def initialize(filename)
-  
+    @cards = []
+    read_cards(filename)
   end
 
+  def read_cards(filename)
+    File.foreach filename do |line|
+      card = line.chomp.split(",")
+      cards << Card.new(card[0], card[1])
+    end
+  end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -3,9 +3,28 @@ require './lib/card_generator'
 
 class ClassGeneratorTest < Minitest::Test
 
-
   def test_it_exists
     card_gen = CardGenerator.new("./cards.txt")
+
     assert_instance_of CardGenerator, card_gen
+  end
+
+  def test_it_can_load_file
+    card_gen = CardGenerator.new("./cards.txt")
+
+    assert File.exists?("./cards.txt")
+  end
+
+  def test_it_can_count_cards
+    card_gen = CardGenerator.new("./cards.txt")
+
+    assert_equal 4, card_gen.cards.count
+  end
+
+  def test_it_holds_cards
+    card_gen = CardGenerator.new("./cards.txt")
+    
+    assert_instance_of Array, card_gen.cards
+    assert_instance_of Card, card_gen.cards[2]
   end
 end
